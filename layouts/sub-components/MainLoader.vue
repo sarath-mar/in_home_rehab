b35245c4ba51d6c162aa92bde70d3f4690d9f9c0
Sarath-Mar <sarathbnm@gmail.com>
2025-02-28 15:46:20 +0530

    updated

--- layouts/sub-components/MainLoader.vue
+++ layouts/sub-components/MainLoader.vue
@@ -9,40 +9,47 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
-.loader-main{
-    height: 100vh;
-    // background-color:#263038 ;
+.loader-main {
+  height: 100vh;
+  // background-color:#263038 ;
 }
 .loader {
-    position: absolute;
-    top: 45%;
-    left: 50%;
-    transform: translate(-50%,-50%);
-    width:  100px;
-    height: 100px;
-    background: #263038;
-    transform: rotateX(65deg) rotate(45deg);
-    // remove bellows command for perspective change
-    //transform: perspective(200px) rotateX(65deg) rotate(45deg); 
-    color: var(--tertiary-text-color);
-    animation: layers1 1s linear infinite alternate;
-  }
-  .loader:after {
-    content: '';
-    position: absolute;
-    inset: 0;
-   
-    background: #de3500;
-    animation: layerTr 1s linear infinite alternate;
-  }
+  position: absolute;
+  width: 100px;
+  height: 100px;
+  background: #263038;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%) rotateX(65deg) rotate(45deg);
+  // remove bellows command for perspective change
+  //transform: perspective(200px) rotateX(65deg) rotate(45deg);
+  color: var(--tertiary-text-color);
+  animation: layers1 1s linear infinite alternate;
+}
+.loader:after {
+  content: "";
+  position: absolute;
+  inset: 0;
 
-  @keyframes layers1 {
-    0% { box-shadow: 0px 0px 0 0px  }
-   90% , 100% { box-shadow: 20px 20px 0 -4px  }
+  background: #de3500;
+  animation: layerTr 1s linear infinite alternate;
+}
+
+@keyframes layers1 {
+  0% {
+    box-shadow: 0px 0px 0 0px;
   }
-  @keyframes layerTr {
-    0% { transform:  translate(0, 0) scale(1) }
-    100% {  transform: translate(-25px, -25px) scale(1) }
+  90%,
+  100% {
+    box-shadow: 20px 20px 0 -4px;
   }
-      
+}
+@keyframes layerTr {
+  0% {
+    transform: translate(0, 0) scale(1);
+  }
+  100% {
+    transform: translate(-25px, -25px) scale(1);
+  }
+}
 </style>
