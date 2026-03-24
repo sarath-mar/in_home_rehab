56b92b545fb5a24d5e0852ac5c7c894af0ea566b
Sarath M <sarath.m@klarion.ai>
2026-03-24 01:20:54 -0400

    image updated 2

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,9 +55,10 @@ export default defineNuxtConfig({
           rel: "canonical",
           href: "https://ashvarexport.com",
         },
-        { rel: "icon", type: "image/png", href: "/images/logo.png" },
-        { rel: "shortcut icon", type: "image/png", href: "/images/logo.png" },
-        { rel: "apple-touch-icon", href: "/images/logo.png" },
+        { rel: "icon", type: "image/png", sizes: "48x48", href: "/favicon-48x48.png" },
+        { rel: "shortcut icon", href: "/favicon.ico" },
+        { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+        { rel: "manifest", href: "/site.webmanifest" },
         { rel: "preconnect", href: "https://fonts.googleapis.com" },
         {
           rel: "preconnect",
